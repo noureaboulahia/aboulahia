958e4e81b8d5ebc776928700d36cc6f668b07a49
noureedine <noure.aboulahia@gmail.com>
2025-03-14 16:30:15 +0100

    inchalah it works

---
+++ cc/myscripts.js
@@ -0,0 +1,301 @@
+var x = false;
+function myAccFuncp() {
+  var x = document.getElementById("demoAccp");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+    x.previousElementSibling.className += " w3-green";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
+    x.previousElementSibling.className = 
+    x.previousElementSibling.className.replace(" w3-green", "");
+  }
+}
+function myAccFunca() {
+  var x = document.getElementById("demoAcca");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+    x.previousElementSibling.className += " w3-green";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
+    x.previousElementSibling.className = 
+    x.previousElementSibling.className.replace(" w3-green", "");
+  }
+}
+function myAccFuncb() {
+  var x = document.getElementById("demoAccb");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+    x.previousElementSibling.className += " w3-green";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
+    x.previousElementSibling.className = 
+    x.previousElementSibling.className.replace(" w3-green", "");
+  }
+}
+function myAccFuncd() {
+  var x = document.getElementById("demoAccd");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+    x.previousElementSibling.className += " w3-green";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
+    x.previousElementSibling.className = 
+    x.previousElementSibling.className.replace(" w3-green", "");
+  }
+}
+function myAccFunce() {
+  var x = document.getElementById("demoAcce");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+    x.previousElementSibling.className += " w3-green";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
+    x.previousElementSibling.className = 
+    x.previousElementSibling.className.replace(" w3-green", "");
+  }
+}
+function myAccFuncf() {
+  var x = document.getElementById("demoAccf");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+    x.previousElementSibling.className += " w3-green";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
+    x.previousElementSibling.className = 
+    x.previousElementSibling.className.replace(" w3-green", "");
+  }
+}
+function myAccFuncg() {
+  var x = document.getElementById("demoAccg");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+    x.previousElementSibling.className += " w3-green";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
+    x.previousElementSibling.className = 
+    x.previousElementSibling.className.replace(" w3-green", "");
+  }
+}
+function myAccFunch() {
+  var x = document.getElementById("demoAcch");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+    x.previousElementSibling.className += " w3-green";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
+    x.previousElementSibling.className = 
+    x.previousElementSibling.className.replace(" w3-green", "");
+  }
+}
+function myAccFunci() {
+  var x = document.getElementById("demoAcci");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+    x.previousElementSibling.className += " w3-green";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
+    x.previousElementSibling.className = 
+    x.previousElementSibling.className.replace(" w3-green", "");
+  }
+}
+function myAccFuncj() {
+  var x = document.getElementById("demoAccj");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+    x.previousElementSibling.className += " w3-green";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
+    x.previousElementSibling.className = 
+    x.previousElementSibling.className.replace(" w3-green", "");
+  }
+}
+function myAccFunck() {
+  var x = document.getElementById("demoAcck");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+    x.previousElementSibling.className += " w3-green";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
+    x.previousElementSibling.className = 
+    x.previousElementSibling.className.replace(" w3-green", "");
+  }
+}
+function myAccFuncl() {
+  var x = document.getElementById("demoAccl");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+    x.previousElementSibling.className += " w3-green";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
+    x.previousElementSibling.className = 
+    x.previousElementSibling.className.replace(" w3-green", "");
+  }
+}
+function myAccFuncm() {
+  var x = document.getElementById("demoAccm");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+    x.previousElementSibling.className += " w3-green";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
+    x.previousElementSibling.className = 
+    x.previousElementSibling.className.replace(" w3-green", "");
+  }
+}
+function myAccFuncn() {
+  var x = document.getElementById("demoAccn");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+    x.previousElementSibling.className += " w3-green";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
+    x.previousElementSibling.className = 
+    x.previousElementSibling.className.replace(" w3-green", "");
+  }
+}
+function myFunctionnnn() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+  var myDropdown = document.getElementById("myDropdown");
+    if (myDropdown.classList.contains('show')) {
+      myDropdown.classList.remove('show');
+    }
+  }
+}
+function openNavS() {
+  var x = document.getElementById("myTopnav");
+  x.className = "topnav";
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+  closeNav();
+}
+
+function closeNavS() {
+  document.getElementById("listbooks").style.display = "block";
+  document.getElementById("myUL").style.display = "none";
+  document.getElementById("myInput").value= "";
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+  
+}
+function openNav() {
+if (Boolean(x)) {
+  closeNav();
+}
+else {
+	document.getElementById("mySidenav").style.width = "0";
+x = true;
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+  closeNavS();
+}}
+
+function closeNav() {
+x = false;
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+  
+}
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+document.addEventListener('copy', (event) => {
+  const pagelink = `\n\n... لمواصلة المطالعة اضغط على الرابط: ${document.location.href}`;
+  event.clipboardData.setData('text', document.getSelection() + pagelink);
+  event.preventDefault();
+});
+
+function addLink() {
+ //Get the selected text and append the extra info
+ var selection = window.getSelection(),
+ pagelink = '<br /><br />  المصدر:   ' + '  ' +document.location.href + '   ', // Change this text
+ copytext = selection + pagelink,
+ newdiv = document.createElement('div');
+
+ //hide the newly created container
+ newdiv.style.position = 'absolute';
+ newdiv.style.left = '-99999px';
+
+ //insert the container, fill it with the extended text, and define the new selection
+ document.body.appendChild(newdiv);
+ newdiv.innerHTML = copytext;
+ selection.selectAllChildren(newdiv);
+
+ window.setTimeout(function () {
+ document.body.removeChild(newdiv);
+ }, 100);
+ }
+
+ document.addEventListener('copy', addLink);
+ function includeHTML() {
+  var z, i, elmnt, file, xhttp;
+  /*loop through a collection of all HTML elements:*/
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain atrribute:*/
+    file = elmnt.getAttribute("w3-include-html");
+    if (file) {
+      /*make an HTTP request using the attribute value as the file name:*/
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+          /*remove the attribute, and call this function once more:*/
+          elmnt.removeAttribute("w3-include-html");
+          includeHTML();
+        }
+      }      
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      /*exit the function:*/
+      return;
+    }
+  }
+}
+function search() {
+    var input, filter, ul, li, a, i, txtValue,listbooks;
+    listbooks=document.getElementById("listbooks");
+    input = document.getElementById("myInput");
+    filter = removehamza(input.value);
+    ul = document.getElementById("myUL");
+    
+    if(filter==""){
+    listbooks.style.display = "block";
+    ul.style.display = "none";
+    }
+    else{
+    listbooks.style.display = "none";
+    ul.style.display = "block";
+    }
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        txtValue = removehamza(a.textContent) || removehamza(a.innerText);
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
+function removehamza(book){
+    result = book.replace("أ", "ا");
+	result = book.replace("إ", "ا");
+	result = book.replace("آ", "ا");
+	return result;
+}
+
